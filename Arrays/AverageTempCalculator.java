65b3fa5cea03b3378232f86dd6803efcfdbbe72c
Noah Devine <Noah.Devine@keyin.com>
2025-05-09 14:30:12 -0230

    Added base code for average temp calculator
    - Added the base code for the average temp calculator assignment
    - Prompts the user to enter the number of days they would like to calculate the average for, gets passed to the temps array
    - Uses a for loop to go through the array and assign values based on the users inputs, adds each input to the sum variable
    
    - Does a simple average calculation using the sum and numDay variable
    
    - Uses another for loop to go through the array of temperatures and compare each one against the caluclated average. If the temp is above the average, the numbersAboveAverage count gets incremented by one. The count is then displayed.

---
+++ Arrays/AverageTempCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class AverageTempCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Collecting the number of days for the array from the user.
+        System.out.println("Please enter the number of days you would like to calculate: ");
+        int numDays = scanner.nextInt();
+
+        // Initialize array, collect temperatures for each day, adding them to sum variable.
+        double[] temps = new double[numDays];
+        double sum = 0;
+
+        for (int i = 0; i < numDays; i++) {
+            System.out.println("Please enter the temperature for day " + (i + 1) + ": ");
+            temps[i] = scanner.nextDouble();
+            sum += temps[i];
+        }
+
+        // Simple average calculation & displaying the number of days above the calculated average
+        double average = sum / numDays;
+        System.out.printf("Average temperature for %d days: %.2f\n", numDays, average);
+
+        int numbersAboveAverage = 0;
+
+        for (int i = 0; i < numDays; i++) {
+            if (temps[i] > average) {
+                numbersAboveAverage ++;
+            }
+        }
+
+        System.out.println("Days above calculated average temperature: " + numbersAboveAverage);
+        scanner.close();
+    }
+}
