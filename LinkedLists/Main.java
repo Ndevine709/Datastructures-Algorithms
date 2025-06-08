29dc7b21a5360ffb32ce4cfd0198cd04274ac6ae
Noah Devine <Noah.Devine@keyin.com>
2025-06-08 15:19:21 -0230

    Added folder and files for linkedList assignment
    - Added needed files and folders for the linkedlist delete method assignment
    - Created delete method within SingleLinkedListTest file which covers all assignment requirments
    - Added code to the Main class file to test the delete method created

---
+++ LinkedLists/Main.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SingleLinkedListTest sll = new SingleLinkedListTest();
+        sll.insertLinkedList(1,0);
+        sll.insertLinkedList(2,1);
+        sll.insertLinkedList(3,2);
+        sll.insertLinkedList(4,3);
+        sll.traverseLinkedList();
+        sll.searchNode(5);
+
+        // Delete method testing below
+        System.out.println("Starting list:");
+        sll.traverseLinkedList();
+
+        // Deleting at an invalid position
+        sll.delete(20);
+
+        // Deleting head
+        sll.delete(0);
+        sll.traverseLinkedList();
+
+        // Deleting from the middle of the list
+        sll.delete(1);
+        sll.traverseLinkedList();
+
+        // Deleting from the tail of the list
+        sll.delete(sll.size - 1);
+        sll.traverseLinkedList();
+
+    }
+}
